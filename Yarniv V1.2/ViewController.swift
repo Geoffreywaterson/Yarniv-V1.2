e8fc9d3a3154ba906316471ed756faefc3009e73
Geoff Waterson <chukkey@gmail.com>
2020-05-20 13:36:11 -0700

    Yarniv V1.2 Project. First Stage: Create TWO random cards, add and display the combined score. Setup in the lower half of the display/screen. This is very hardcoded, and does not account for different number of cards (i.e. 3, 4 or 5 cards in a hand) -- overcoming this will be the next step

--- Yarniv V1.2/ViewController.swift
+++ Yarniv V1.2/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LeftCardImage: UIImageView!
+    @IBOutlet weak var RightCardImage: UIImageView!
+    @IBOutlet weak var ScoreLabel: UILabel!
+      
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func DealTapped(_ sender: Any) {
+        
+        print("Deal button tapped")
+        
+        // What happens to the Left Card
+        let LeftScoreRandomNumber = arc4random_uniform(13) + 1
+        print("Left Score Random Number = \(LeftScoreRandomNumber)")
+        
+        LeftCardImage.image = UIImage(named: "card\(LeftScoreRandomNumber)")
+        
+        // What happens to the Right Card
+        let RightScoreRandomNumber = arc4random_uniform(13) + 1
+        print("Right Score Random Number = \(RightScoreRandomNumber)")
+        
+        RightCardImage.image = UIImage(named: "card\(RightScoreRandomNumber)")
+        
+        
+        // What happens to the Score Label
+        let TotalScore = LeftScoreRandomNumber + RightScoreRandomNumber
+        ScoreLabel.text = ("\(TotalScore)")
+        
+        
+    }
+    
+    
+    
+    
+    
 }
 
