7870ae1030bd821a4cb97c983def4f7b229347c7
Geoff Waterson <chukkey@gmail.com>
2020-05-31 19:50:38 -0700

    Updated CardModel to take an array, and then take a single card and add it to the index. This avoids duplicates

--- Yarniv V1.2/CardModel.swift
+++ Yarniv V1.2/CardModel.swift
@@ -16,26 +16,37 @@ class CardModel {
         
         // declare an array to store generated numbers
         var generatedCardsArray = [Card]()
-        var cardListArray = [String]()
+        // var cardListArray = [String]()
+        
+        var allCards = ["card1", "card2", "card3", "card4", "card5", "card6", "card7", "card8", "card9", "card10", "card11", "card12", "card13"]
+        allCards.shuffle()
+        print(allCards)
+        var initialNumber = 0
         
         // randomly generate card numbers
         for _ in 1...5 {
             // Get a random Number
-            let randomNumber = arc4random_uniform(13) + 1
+            // let randomNumber = arc4random_uniform(13) + 1
             
             // Log the number
-            print(randomNumber)
+            // print(randomNumber)
         
+            // Instead of 'randomNumber', we need this to be the first or next card within the deck of cards. So make it randomCard instead of randomNumber
+            
+            
             // Create the Card Object
             let cardOne = Card()
-            cardOne.imageName = "card\(randomNumber)"
+            cardOne.imageName = (allCards[initialNumber])
+            // cardOne.imageName = "card\(randomNumber)"
             
-            let CardOneItem = "card\(randomNumber)"
-            print(CardOneItem)
+            // let CardOneItem = "card\(randomNumber)"
+            // print(CardOneItem)
             
             // Add the card to the Array
             generatedCardsArray.append(cardOne)
-            cardListArray.append(CardOneItem)
+            // cardListArray.append(CardOneItem)
+            
+            initialNumber += 1
             
             // OPTIONAL TODO: Make sure only unique pairs (While Loop?)
             
@@ -45,9 +56,10 @@ class CardModel {
         // MAY NOT BE NEEDED: randomize the array
         
         // Return the array
-        print(cardListArray)
-        cardListArray.sort()
-        print(cardListArray)
+        // print(generatedCardsArray)
+        // print(cardListArray)
+        // cardListArray.sort()
+        // print(cardListArray)
 
         return generatedCardsArray
                 
